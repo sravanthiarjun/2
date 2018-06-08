f9058a80ff17bda80f24e904ed86ad84a063e0f5
sravanthiarjun <38848713+sravanthiarjun@users.noreply.github.com>
2018-06-08 09:02:06 +0530

    Update media.py

--- media.py
+++ media.py
@@ -2,11 +2,13 @@ import webbrowser
 
 
 class Movie():
-    """this is the class i can use movie() and all so public it access any where throught out the program so in that class we have to declare some attributes like that so it will be dispalyed """
+    """this is the class i can use movie() and all so public it access any where
+    ""throught out the program so in that class we have to declare some
+    ""attributes like that so it will be dispalyed """
 
     def __init__(
         i,
-        movie_title, 
+        movie_title,
         storyline,
         poster_image,
         trailer_youtube
